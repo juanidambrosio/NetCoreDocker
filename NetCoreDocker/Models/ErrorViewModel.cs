c2bf71f35360396e38011ac92fd334322525748f
Juan DAmbrosio <dambrosiojuani@gmail.com>
2018-04-25 15:36:39 -0300

    Add project files.

---
+++ NetCoreDocker/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NetCoreDocker.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
